efa666e79cb100e8f9ade964c0c80f26a76759dc
imsimgaa022 <m.simgaa@gmail.com>
2022-07-05 19:19:13 +0200

    migration

--- database/migrations/2022_07_05_161918_create_cars_table.php
+++ database/migrations/2022_07_05_161918_create_cars_table.php
@@ -16,6 +16,13 @@ return new class extends Migration
         Schema::create('cars', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('brand');
+            $table->string('model');
+            $table->integer('year');
+            $table->integer('max_speed');
+            $table->boolean('is_automatic');
+            $table->string('engine');
+            $table->integer('number_of_doors');
         });
     }
 
